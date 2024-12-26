d3b39349ec95113dd44bf263c27959ccbb1d1ee6
Ratnesh chaturvedi <ratnesh16@gmail.com>
2024-12-26 21:23:33 +0530

    added

---
+++ Array/Buy-sell-stock-maxprofit/main.cpp
@@ -0,0 +1,37 @@
+// Problem statement
+// You are given an array/list 'prices' where the elements of the array represent the prices of the stock as they were yesterday and indices of the array represent minutes. Your task is to find and return the maximum profit you can make by buying and selling the stock. You can buy and sell the stock only once.
+// Note:
+// You can’t sell without buying first.
+// For Example:
+// For the given array [ 2, 100, 150, 120],
+// The maximum profit can be achieved by buying the stock at minute 0 when its price is Rs. 2 and selling it at minute 2 when its price is Rs. 150.
+// So, the output will be 148.
+
+
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    // Brute force
+int n=prices.size();
+    // int maxx=INT_MIN;
+    // for(int i=0;i<n;i++){
+    //     int buy=prices[i];
+    //     for(int j=i+1;j<n;j++){
+    //         int sell=prices[j];
+    //         maxx=max(maxx,sell-buy);
+    //     }
+    // }
+    // if(maxx<0)
+    // return 0;
+    // return maxx;
+
+    //Optimal Approach
+
+    int mini=prices[0];
+    int profit=0;
+    for(int i=1;i<n;i++){
+        int cost=prices[i]-mini;
+        profit=max(profit,cost);
+        mini=min(mini,prices[i]);
+    }
+    return profit;
+}
---
+++ Array/maximum-sub-array-sum-Kadane's algo/main.cpp
@@ -0,0 +1,48 @@
+/*
+Problem statement
+You are given an array 'arr' of length 'n', consisting of integers.
+A subarray is a contiguous segment of an array. In other words, a subarray can be formed by removing 0 or more integers from the beginning and 0 or more integers from the end of an array.
+Find the sum of the subarray (including empty subarray) having maximum sum among all subarrays.
+The sum of an empty subarray is 0.
+Example :
+Input: 'arr' = [1, 2, 7, -4, 3, 2, -10, 9, 1]
+Output: 11
+Explanation: The subarray yielding the maximum sum is [1, 2, 7, -4, 3, 2].
+*/
+
+
+
+
+
+
+
+
+long long maxSubarraySum(vector<int> arr, int n)
+{
+    //Better Approach
+    // int m=INT_MIN;
+    
+    // for(int i=0;i<n;i++){
+    // int sum=0;
+    //     for(int j=i;j<n;j++){
+    //        sum+=arr[j];
+    //     m=max(sum,m);
+    //     }
+    // }
+    // if(m>0)
+    // return m;
+    // return 0;
+
+
+//Optimised -KADANE ALGO
+long long sum=0,maximum=LONG_MIN;
+
+for(int i=0;i<n;i++){
+    sum+=arr[i];
+    if(sum<0){
+        sum=0;
+    }
+    maximum=max(maximum,sum);
+}
+return maximum;
+}
