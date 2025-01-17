e0ab798b4aa0a5d4e9715f0aec87002d845a262b
Ratnesh chaturvedi <ratnesh16@gmail.com>
2025-01-17 20:42:43 +0530

    change

--- BINARY-SEARCH/Nth-root-of-M/main.cpp
+++ BINARY-SEARCH/Nth-root-of-M/main.cpp
@@ -1,24 +1,22 @@
  Find Nth Root Of M
  Problem statement
-You are given two positive integers 'n' and 'm'. You have to return the 'nth' root of 'm', i.e. 'm(1/n)'. If the 'nth root is not an integer, return -1.
-Note:
-'nth' root of an integer 'm' is a number, which, when raised to the power 'n', gives 'm' as a result.
-Example:
-Input: ‘n’ = 3, ‘m’ = 27
-Output: 3
-Explanation: 
-3rd Root of 27 is 3, as (3)^3 equals 27.
+// You are given two positive integers 'n' and 'm'. You have to return the 'nth' root of 'm', i.e. 'm(1/n)'. If the 'nth root is not an integer, return -1.
+// Note:
+// 'nth' root of an integer 'm' is a number, which, when raised to the power 'n', gives 'm' as a result.
+// Example:
+// Input: ‘n’ = 3, ‘m’ = 27
+// Output: 3
+// Explanation: 
+// 3rd Root of 27 is 3, as (3)^3 equals 27.
 
-Sample Input 2:
-4 69
+// Sample Input 2:
+// 4 69
+// Sample Output 2:
+// -1
+// Explanation For Sample Input 2:
+// 4th Root of 69 is not an integer, hence -1.
 
 
-Sample Output 2:
--1
-
-
-Explanation For Sample Input 2:
-4th Root of 69 is not an integer, hence -1.
 
 
 #include <bits/stdc++.h>
