3dcd0555085703c94bdd486fe4f89d645e561542
Ratnesh chaturvedi <ratnesh16@gmail.com>
2025-04-27 23:41:40 +0530

    added

--- Stack And Queue/Monotonic-Stack-and-Queue-Vimp.cpp
+++ Stack And Queue/Monotonic-Stack-and-Queue-Vimp.cpp
@@ -64,3 +64,257 @@ vector<int> nextGreaterElements(vector<int>& nums) {
    }
    return ans;
 }
+
+
+//Find nearest smaller element on the left side
+void smaller(vector<int>&arr){
+    stack<int>st;
+    int n=arr.size();
+    int mini=INT_MAX;
+    vector<int>ans(n,-1);
+  
+    for(int i=0;i<n;i++){
+        while(!st.empty() && st.top()>=arr[i]) st.pop();
+        if(st.empty()){
+            ans[i]=-1;
+            
+        }
+        else ans[i]=st.top();
+        st.push(arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=ans[i];
+    }
+  
+    
+}
+
+//next smallest Element
+vector<int>nextSmallEl(vector<int>&arr){
+    int n=arr.size();
+    stack<int>st;
+    vector<int>ans(n);
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && st.top()>=arr[i]) st.pop();
+        if(st.empty()) ans[i]=-1;
+        else ans[i]=st.top();
+        st.push(arr[i]);
+    }
+    return ans;
+}
+
+
+// previous smallest Element
+vector<int>prevSmallEl(vector<int>&arr){
+    int n=arr.size();
+    stack<int>st;
+    vector<int>ans(n);
+    for(int i=0;i<n;i++){
+        while(!st.empty() && st.top()>=arr[i]) st.pop();
+        if(st.empty()) ans[i]=-1;
+        else ans[i]=st.top();
+        st.push(arr[i]);
+    }
+    return ans;
+}
+
+
+
+// 42. Trapping Rain Water
+//TC-O(3n) SC-O(2n) 
+//little Optimisation in space can we done by replacing the prefix max SC-O(N); 
+
+
+
+int trap(vector<int>& height) {
+        int n=height.size();
+        // vector<int>prefixMax(n);
+        vector<int>suffixMax(n);
+        // prefixMax[0]=height[0];
+        // for(int i=1;i<n;i++){
+        //     prefixMax[i]=max( prefixMax[i-1],height[i]);
+        // }
+        suffixMax[n-1]=height[n-1];
+           for(int i=n-2;i>=0;i--){
+            suffixMax[i]=max( suffixMax[i+1],height[i]);
+        }
+          int total=0;
+           int maxi=INT_MIN;
+          for(int i=0;i<n;i++){
+            //for space Optimisation
+                 maxi=max(height[i],maxi);
+            total+=min(maxi,suffixMax[i])-height[i];
+            //by using prefixMax;
+            // total+=min(prefixMax[i],suffixMax[i])-height[i];
+
+          }
+ return total;
+    }
+
+
+    // optimla -Two pointer 
+    // TC-O(N) SC-O(1)
+     int trap(vector<int>& height) {
+        int n=height.size();
+       int total=0;
+       int lmax=0,rmax=0,l=0,r=n-1;
+       while(l<r){
+        if(height[l]<=height[r]){
+            if(lmax>height[l]) total+=lmax-height[l];
+            else lmax=height[l];
+            l++;
+        }
+        else {
+            if(rmax>height[r]) total+=rmax-height[r];
+            else rmax=height[r];
+            r--;
+        }
+       }
+       
+          
+ return total;
+    }
+
+
+    // 907. Sum of Subarray Minimums
+    //Brute 
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        int sum=0;
+        int mod=(int)(1e9 + 7);
+        for(int i=0;i<n;i++){
+            int mini=arr[i];
+            for(int j=i;j<n;j++){
+           mini=min(arr[j],mini);
+           sum=(sum+mini)%mod;
+            }
+        }
+        return sum;
+    }
+
+    //optimal
+    vector<int>findNse(vector<int>&arr){
+        int n=arr.size();
+        vector<int>nse(n);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+     while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+        nse[i]=st.empty()?n:st.top();
+        st.push(i);
+        }
+        return nse;
+    }
+    vector<int>findPsee(vector<int>&arr){
+        int n=arr.size();
+        vector<int>psee(n);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+     while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+        psee[i]=st.empty()?-1:st.top();
+        st.push(i);
+        }
+        return psee;
+    }
+    
+        int sumSubarrayMins(vector<int>& arr) {
+         vector<int>nse=findNse(arr);
+         vector<int>psee=findPsee(arr);
+         int total=0;
+         int mod=(int)(1e9+7);
+         for(int i=0;i<arr.size();i++){
+            int left=i-psee[i];
+            int right=nse[i]-i;
+            total=(total+(right*left*1LL*arr[i])%mod)%mod;
+         }
+         return total;
+        }
+
+
+        // 2104. Sum of Subarray Ranges
+        // Brute force
+        long long subArrayRanges(vector<int>& arr) {
+            int n=arr.size();
+            long long sum=0;
+            for(int i=0;i<n;i++){
+                int maxi=arr[i];
+                int mini=arr[i];
+                for(int j=i;j<n;j++){
+                    maxi=max(maxi,arr[j]);
+                    mini=min(mini,arr[j]);
+                    sum+=(maxi-mini);
+                }
+            }
+            return sum;
+        }
+
+        //optimal
+        vector<int>findNge(vector<int>&arr){
+            int n=arr.size();
+            stack<int>st;
+            vector<int>ans(n);
+            for(int i=n-1;i>=0;i--){
+                while(!st.empty() && arr[st.top()]<=arr[i]) st.pop();
+                ans[i]=st.empty()?n:st.top();
+                st.push(i);
+            }
+            return ans;
+        }
+           vector<int>findPgee(vector<int>&arr){
+                int n=arr.size();
+                vector<int>pgee(n);
+                stack<int>st;
+                for(int i=0;i<n;i++){
+             while(!st.empty() && arr[st.top()]<arr[i]) st.pop();
+                pgee[i]=st.empty()?-1:st.top();
+                st.push(i);
+                }
+                return pgee;
+            }
+             long long sumSubarrayMins(vector<int>& arr) {
+                 vector<int>nse=findNse(arr);
+                 vector<int>psee=findPsee(arr);
+                 long long total=0;
+                 
+                 for(int i=0;i<arr.size();i++){
+                     long long left = i - psee[i];
+                    long long right = nse[i] - i;
+                    total += (left * right * (long long)arr[i]);
+                 }
+                 return total;
+                }
+                 long long  sumSubarrayMax(vector<int>& arr) {
+                 vector<int>nge=findNge(arr);
+                 vector<int>pgee=findPgee(arr);
+                 long long total=0;
+                 for(int i=0;i<arr.size();i++){
+                     long long left = i - pgee[i];
+                    long long right = nge[i] - i;
+                    total += (left * right * (long long)arr[i]);
+                 }
+                 return total;
+                }
+            long long subArrayRanges(vector<int>& arr) {
+                return  sumSubarrayMax(arr)*1LL-sumSubarrayMins(arr)*1LL;
+            }
+
+
+
+            // 735. Asteroid Collision
+            vector<int> asteroidCollision(vector<int>& arr) {
+                int n = arr.size();
+                vector<int> st;
+                for (int i = 0; i < n; i++) {
+                    if (arr[i] > 0)
+                        st.push_back(arr[i]);
+                    else {
+                        while (!st.empty() && st.back() > 0 &&
+                               (abs(arr[i]) > st.back()))
+                            st.pop_back();
+                        if (!st.empty() && st.back() == abs(arr[i]))
+                            st.pop_back();
+                        else if (st.empty() || st.back() < 0)
+                            st.push_back(arr[i]);
+                    }
+                }
+                return st;
+            }
