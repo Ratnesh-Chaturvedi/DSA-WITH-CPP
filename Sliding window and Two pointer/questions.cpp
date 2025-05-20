9729b8bd8333d185293018f060c5cd9edf07b1ff
Ratnesh chaturvedi <ratnesh16@gmail.com>
2025-05-21 00:28:41 +0530

    added

--- Sliding window and Two pointer/questions.cpp
+++ Sliding window and Two pointer/questions.cpp
@@ -247,3 +247,75 @@ int cntsubarray(vector<int>& nums, int k){
 return cntsubarray(nums,k)-cntsubarray(nums,k-1);
     }
 
+
+// 1358. Number of Substrings Containing All Three Characters
+    int numberOfSubstrings(string s) {
+        int n=s.size();
+        //Brute force
+    //     int cnt=0;
+    //     for(int i=0;i<n;i++){
+    //         set<char>st;
+    //         for(int j=i;j<n;j++){
+    //             st.insert(s[j]);
+    //             if(st.size()==3){
+    //                 cnt++;
+    //             }
+    //         }
+    //     }
+    //     return cnt;
+  
+  // better 
+//  int l=0,r=0,cnt=0;
+//  unordered_map<char,int>hash;
+//  while(r<n){
+//     hash[s[r]]++;
+// while(hash.size()==3){
+//         hash[s[l]]--;
+//         if(hash[s[l]]==0) hash.erase(s[l]);
+//         l++;
+//     cnt=cnt+n-(r);
+//     }    
+//     r++;
+//  }
+//  return cnt;
+
+
+// optimised
+int i=0;
+vector<int>hash(3,-1);
+int cnt=0;
+while(i<n){
+    hash[s[i]-'a']=i;
+ if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
+    int mini=min(hash[0],min(hash[1],hash[2]));
+    cnt+=mini+1;
+ }
+ i++;
+}
+return cnt;
+    }
+
+
+//992. Subarrays with K Different Integers
+// same like binary subarray with sum / count no. of subarray equal k
+int count(vector<int>& nums, int k){
+    if(k<0) return 0;
+     int r=0,l=0,cnt=0;
+        int n=nums.size();
+        unordered_map<int,int>hash;
+        while(r<n){
+         hash[nums[r]]++;
+         while(hash.size()>k){
+         
+            hash[nums[l]]--;
+            if(hash[nums[l]]==0) hash.erase(nums[l]);
+            l++;
+         }
+        cnt+=r-l+1;
+         r++;
+        }
+        return cnt;
+}
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+       return count(nums,k)-count(nums,k-1);
+    }
